16dfafea2630b37fc48906bf90ca9f750a0b701a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 17:32:23 -0500

    add announcment above navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Announcement from '../components/Announcement';
 import NavBar from '../components/NavBar';
 import Slider from '../components/Slider';
 
 const Home = () => {
   return (
     <>
+      <Announcement />
       <NavBar />
       <Slider />
     </>
