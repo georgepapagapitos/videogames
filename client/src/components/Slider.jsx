d2cebad46b8c4eec788a82d4ad1158ea9750f930
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 15:50:51 -0500

    creating arrow component within slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,9 +1,29 @@
 import styled from 'styled-components';
+import {
+  ArrowLeftOutlinedIcon,
+  ArrowRightOutlinedIcon,
+} from '@mui/icons-material';
 
-const Container = styled.div``;
+const Container = styled.div`
+  border: 1px solid red;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+`;
+
+const Arrow = styled.div``;
 
 const Slider = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Arrow>
+        <ArrowLeftOutlinedIcon />
+      </Arrow>
+      <Arrow>
+        <ArrowRightOutlinedIcon />
+      </Arrow>
+    </Container>
+  );
 };
 
 export default Slider;
