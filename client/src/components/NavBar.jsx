ccf63ce6f607b136cbd49ccb77fbcbfe4ace0f98
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 15:33:46 -0500

    navbar contents created

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   height: 60px;
-  border: 1px solid black;
 `;
 
 const Wrapper = styled.div`
@@ -18,9 +17,25 @@ const Left = styled.div`
   align-items: center;
 `;
 
+const SearchContainer = styled.div`
+  border: 0.5px solid gray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
 const Center = styled.div`
   flex: 1;
   text-align: center;
+`;
+
+const Heading = styled.h1`
+  font-weight: bold;
 `;
 
 const Right = styled.div`
@@ -30,13 +45,28 @@ const Right = styled.div`
   justify-content: flex-end;
 `;
 
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
 const NavBar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>Left</Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Left>
+          <SearchContainer>
+            <Input placeholder="Search..." />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Heading>GAMES.</Heading>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
