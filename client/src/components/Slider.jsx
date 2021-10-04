6f24410b22d020db8dea6769f2b8d99aec1fd63a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 16:18:10 -0500

    style of slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -29,21 +29,51 @@ const Arrow = styled.div`
   z-index: 2;
 `;
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+`;
 
-const Slide = styled.div``;
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bgColor};
+`;
 
-const ImageContainer = styled.div``;
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
 
-const Image = styled.img``;
+const Image = styled.img`
+  height: 80%;
+`;
 
-const InfoContainer = styled.div``;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 70px;
+`;
 
-const Description = styled.p``;
+const Description = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 // Slider component
 const Slider = () => {
