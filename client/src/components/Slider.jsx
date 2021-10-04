41245a2e38296685c2ce323d34084967dd11a638
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 16:11:14 -0500

    created wrapper + slider components

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -16,16 +16,55 @@ const Arrow = styled.div`
   height: 50px;
   background-color: #ffffffe2;
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === 'left' && '10px'};
+  right: ${(props) => props.direction === 'right' && '10px'};
+  margin: auto;
+  cursor: pointer;
+  z-index: 2;
 `;
+
+const Wrapper = styled.div``;
+
+const Slide = styled.div``;
+
+const ImageContainer = styled.div``;
+
+const Image = styled.img``;
+
+const InfoContainer = styled.div``;
+
+const Title = styled.h1``;
+
+const Description = styled.p``;
+
+const Button = styled.button``;
 
 // Slider component
 const Slider = () => {
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left">
         <ArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+        <Slide>
+          <ImageContainer>
+            <Image />
+          </ImageContainer>
+          <InfoContainer>
+            <Title>GAME TITLE</Title>
+            <Description>GAME DESCRIPTION</Description>
+            <Button>MORE INFO</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
         <ArrowRightOutlinedIcon />
       </Arrow>
     </Container>
