d89cd3a10e305f132ff80de25f5f6ace4a037528
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 13:47:06 -0500

    adding flex properties to navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 60px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`;
+
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>Left</Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default NavBar;
