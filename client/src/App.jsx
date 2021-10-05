2b2ab229f7533e8c59002c01432e575006445e8a
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-05 10:48:53 -0500

    routing with react-router-dom + fake user for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+// Local imports
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+// App component
 function App() {
-  return <Register />;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
