0378c195adb27e48d13af0c92bd04149c69f3ce4
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-04 19:13:49 -0500

    newsletter component styled

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -11,23 +11,45 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 70px;
+  margin-bottom: 20px;
+`;
 
-const Description = styled.div``;
+const Description = styled.div`
+  font-size: 24px;
+  font-weight: 300;
+  margin-bottom: 20px;
+`;
 
-const InputContainer = styled.div``;
+const InputContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid lightgray;
+`;
 
-const Input = styled.input``;
+const Input = styled.input`
+  border: none;
+  flex: 8;
+  padding-left: 20px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  flex: 1;
+  border: none;
+  color: white;
+  background-color: coral;
+`;
 
 const Newsletter = () => {
   return (
     <Container>
-      <Title></Title>
-      <Description></Description>
+      <Title>NEWSLETTER</Title>
+      <Description>GET UPDATES ON YOUR FAVORITE GAMES</Description>
       <InputContainer>
-        <Input />
+        <Input placeholder="Enter your email..." />
         <Button>
           <SendIcon />
         </Button>
