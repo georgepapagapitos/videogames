0c0f26dbab6a6ebcad1b3d1e48e9a2a49c356e32
George Papagapitos <george.g.papagapitos@gmail.com>
2021-10-05 11:09:17 -0500

    added responsive style to game list component

--- client/src/pages/GameList.jsx
+++ client/src/pages/GameList.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 // Local imports
 import NavBar from '../components/NavBar';
@@ -21,17 +22,20 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({ margin: '0px 20px', display: 'flex', flexDirection: 'column' })};
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin-right: 20px;
+  ${mobile({ marginRight: '0px' })};
 `;
 
 const Select = styled.select`
   padding: 10px;
   margin-right: 20px;
+  ${mobile({ margin: '10px 0px' })};
 `;
 
 const Option = styled.option``;
